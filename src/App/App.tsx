eb5216e6b82b8dcde79f28c9df2fcb06bca44c0a
fadlnndd <fanny.delnondedieu@gmail.com>
2023-11-29 14:42:19 +0100

    add proptypes in Button

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,7 +6,15 @@ function App() {
   return (
     <div className="App">Hello World !
       <hr />
-      <Button/>
+      <Button text="toto" type="reset"/>
+      <Button text="tata" type="submit"/>
+      <Button text="titi" 
+        size={{
+          margin:{bottom:'12px', right: '24px'}, 
+          padding: { h: "20px", w: "40px"}
+        }} 
+      />
+      <Button text="tutu" type="button"/>
     </div>
   );
 }
--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import styles from './Button.module.css'
+import PropTypes from 'prop-types'
 
 function Button(props) {
-    return <button className={styles.Button}>Benjamin</button>
+    console.log(props);
+    return <button type={props.type} className={styles.Button}>{props.text}</button>
+}
+Button.propTypes={
+    text: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['submit','rest','button']),
+    size: PropTypes.shape({
+      
+        margin: PropTypes.shape({
+          bottom: PropTypes.string,
+          right: PropTypes.string
+        }),
+        padding: PropTypes.shape({
+          h: PropTypes.string,
+          w: PropTypes.string
+        })
+    })
 }
 
 export default Button;
