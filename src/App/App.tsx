bda8c241d0309db43a1fa43b659b43b80e4a9a01
fadlnndd <fanny.delnondedieu@gmail.com>
2023-11-29 12:14:38 +0100

    add button

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
-    <div className="App">Hello World !</div>
+    <div className="App">Hello World !
+      <hr />
+      <Button/>
+    </div>
   );
 }
 
---
+++ src/App/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Button(props) {
+    return <button>Benjamin</button>
+}
+
+export default Button;
