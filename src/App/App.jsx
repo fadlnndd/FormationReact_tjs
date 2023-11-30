9dd5f772be74f726ca3201005e2a860959ad6262
fadlnndd <fanny.delnondedieu@gmail.com>
2023-11-30 09:51:09 +0100

    prettier

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
-import Button from './components/Button/Button';
+import React from "react";
+import "./App.css";
+import Button from "./components/Button/Button";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={counter:0};
+    this.state = { counter: 0 };
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('state',prevState, this.state)
+    console.log("state", prevState, this.state);
   }
 
   render() {
@@ -17,12 +17,16 @@ class App extends React.Component {
       <div className="App">
         valeur {this.state.counter}
         <hr />
-        <Button text="ajouter 1" type="button" onButtonClicked={()=>{
-          // alert('le button est clicked');
-          this.setState({counter:this.state.counter+1})
-          // console.log(this.state.counter);
-        }} />
-        <Button text="enlever 1" type="button"/>
+        <Button
+          text="ajouter 1"
+          type="button"
+          onButtonClicked={() => {
+            // alert('le button est clicked');
+            this.setState({ counter: this.state.counter + 1 });
+            // console.log(this.state.counter);
+          }}
+        />
+        <Button text="enlever 1" type="button" />
       </div>
     );
   }
--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -1,53 +1,57 @@
 import React, { useEffect, useState } from "react";
-import styles from './Button.module.css'
-import PropTypes from 'prop-types'
+import styles from "./Button.module.css";
+import PropTypes from "prop-types";
 
 function Button(props) {
-    console.log(props);
-    const [isClicked, setIsClicked] = useState({clickedState:false,info2:'coucou'})
-    useEffect(() => {
-      // la valeur a changer
-      if (isClicked.clickState) {
-        setTimeout(() => {
-          setIsClicked({ ...isClicked, clickedState: false});
-        }, 250);        
-      }
-    }, [isClicked])
-    return (
-      <button
-        onClick={(evt) => {
+  console.log(props);
+  const [isClicked, setIsClicked] = useState({
+    clickedState: false,
+    info2: "coucou",
+  });
+  useEffect(() => {
+    // la valeur a changer
+    if (isClicked.clickedState) {
+      setTimeout(() => {
+        setIsClicked({ ...isClicked, clickedState: false });
+      }, 250);
+    }
+  }, [isClicked]);
+  return (
+    <button
+      onClick={(evt) => {
         //   console.log(evt);
-          setIsClicked({ ...isClicked, clickedState: true});
-          if (props.onButtonClicked !== undefined) {
-            props.onButtonClicked();
-          }
-        }}
-        type={props.type}
-        className={styles.Button + (isClicked.clickedState ? ' ' + styles.clicked : '' )}
-        style={{ backgroundColor: props.bgColor }}
-      >
-        {props.text}
-      </button>
-    );
+        setIsClicked({ ...isClicked, clickedState: true });
+        if (props.onButtonClicked !== undefined) {
+          props.onButtonClicked();
+        }
+      }}
+      type={props.type}
+      className={
+        styles.Button + (isClicked.clickedState ? " " + styles.clicked : "")
+      }
+      style={{ backgroundColor: props.bgColor }}
+    >
+      {props.text}
+    </button>
+  );
 }
-Button.propTypes={
-    text: PropTypes.string.isRequired,
-    type: PropTypes.oneOf(['submit','reset','button']),
-    size: PropTypes.shape({
-      
-        margin: PropTypes.shape({
-          bottom: PropTypes.string,
-          right: PropTypes.string
-        }),
-        padding: PropTypes.shape({
-          h: PropTypes.string,
-          w: PropTypes.string
-        })
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["submit", "reset", "button"]),
+  size: PropTypes.shape({
+    margin: PropTypes.shape({
+      bottom: PropTypes.string,
+      right: PropTypes.string,
     }),
-    bgColor: PropTypes.string
-}
+    padding: PropTypes.shape({
+      h: PropTypes.string,
+      w: PropTypes.string,
+    }),
+  }),
+  bgColor: PropTypes.string,
+};
 
-Button.defaultProps={
-    type:'button'
-}
+Button.defaultProps = {
+  type: "button",
+};
 export default Button;
