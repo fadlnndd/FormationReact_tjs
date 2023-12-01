e7b9ca5a69ed4d16ce5cc3ed8867e951bc20abc2
fadlnndd <fanny.delnondedieu@gmail.com>
2023-12-01 17:15:07 +0100

    router bootstrap

--- src/App/components/uis/Navbar/Navbar.jsx
+++ src/App/components/uis/Navbar/Navbar.jsx
@@ -4,25 +4,28 @@ import "../../../../../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../../../../../node_modules/bootstrap/dist/js/bootstrap.js";
 import { Container, Nav, Navbar as NavBar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-// import { LinkContainer } from "react-router-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Navbar = (props) => {
   return (
     <div className={styles.Navbar} data-testid="Navbar">
       <NavBar bg="primary" data-bs-theme="dark">
         <Container>
-          {/* <LinkContainer to="/">
+          <LinkContainer to="/">
 
           <NavBar.Brand>Home</NavBar.Brand>
-          </LinkContainer> */}
-          <Link to="/">Home</Link>
+          </LinkContainer>
           <Nav className="me-auto">
-            <Link to="/thumbnail">List</Link>
-            {/* <Nav.Link href="#list">List</Nav.Link> */}
-            <Link to="/new">New</Link>
-            <Link to="/editor/1">Editor</Link>
-            {/* <Nav.Link href="#new">New</Nav.Link> */}
-            {/* <Nav.Link href="#editor">Editor</Nav.Link> */}
+          <LinkContainer to="/thumbnail">
+            <Nav.Link href="#list">List</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/new">
+
+            <Nav.Link href="#new">New</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/editor/1">
+            <Nav.Link href="#editor">Editor</Nav.Link>
+          </LinkContainer>
           </Nav>
         </Container>
       </NavBar>
